8fcf76c6b24ffb4abe0e911d1531411a54ebef3d
youneedanemail <youneedanemail@gmail.com>
2024-03-21 08:33:14 -0600

    Integrated bootstrap with pagination

--- TheWaterProject/Infrastructure/PaginationTagHelper.cs
+++ TheWaterProject/Infrastructure/PaginationTagHelper.cs
@@ -23,6 +23,15 @@ namespace TheWaterProject.Infrastructure
         public string? PageAction { get; set; }     // same as page-action in html
         public PaginationInfo PageModel { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass {  get; set; } = String.Empty;
+
+        public string PageClassNormal { get; set; } = String.Empty;
+
+        public string PageClassSelected {  get; set; } = String.Empty;
+
+
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if(ViewContext != null && PageModel != null)
@@ -35,7 +44,12 @@ namespace TheWaterProject.Infrastructure
                 { 
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { pageNum = i });
-
+                    
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
 
                     result.InnerHtml.AppendHtml(tag);
